8b6db9f5fd7e9caa6622d09a423bb3b80447f388
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-04-26 08:41:46 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,14 +14,14 @@ var firebaseConfig = {
 
 user_name = localStorage.getItem("user_name");
 
-document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+document.getElementById("user_name").innerHTML = "¡Bienvenido " + user_name + "!";
 
 function addRoom()
 {
   room_name = document.getElementById("room_name").value;
 
   firebase.database().ref("/").child(room_name).update({
-    purpose : "adding room name"
+    purpose : "agregando el nombre de la sala"
   });
 
     localStorage.setItem("room_name", room_name);
@@ -31,7 +31,7 @@ function addRoom()
 
 function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
        Room_names = childKey;
-       console.log("Room Name - " + Room_names);
+       console.log("Nombre de la sala - " + Room_names);
       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
       document.getElementById("output").innerHTML += row;
     });
