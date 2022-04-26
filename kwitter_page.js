dc840cb741e73bfb029cf03fa1ba7ad536fa0b2a
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-04-26 09:11:09 -0500

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -37,7 +37,7 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Me gusta: "+ like +"</span></button><hr>";
 
         row = name_with_tag + message_with_tag +like_button + span_with_tag;       
         document.getElementById("output").innerHTML += row;
