bc1043c14e8841ad325e9985551c3b3d8db3e5ff
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-04-26 09:25:18 -0500

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -28,7 +28,7 @@ function send()
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
-//Start code
+//Inicia el código
          console.log(firebase_message_id);
 	       console.log(message_data);
 	       name = message_data['name'];
@@ -41,13 +41,13 @@ like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="
 
         row = name_with_tag + message_with_tag +like_button + span_with_tag;       
         document.getElementById("output").innerHTML += row;
-//End code
+//Finaliza el código
       } });  }); }
 getData();
 
 function updateLike(message_id)
 {
-  console.log("clicked on like button - " + message_id);
+  console.log("clic en el botón Me gusta - " + message_id);
 	button_id = message_id;
 	likes = document.getElementById(button_id).value;
 	updated_likes = Number(likes) + 1;
